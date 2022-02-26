9b1c85e312ddbd0d837248517589ce2e300f3539
Erin Ewart <ems1980@hotmail.com>
2022-02-26 14:34:49 -0500

    add event listener to save button and save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
-var currentDay = document.querySelector("#currentDay")
+var currentDay = document.querySelector("#currentDay");
 var currentTime = moment();
 
-currentDay.textContent = currentTime.format("MMMM DD, YYYY hh:mm a")
+//Add current day and time to the scheduler
+currentDay.textContent = currentTime.format("MMMM DD, YYYY hh:mm a");
+
+
+// Add event listener to save button
+
+$(".saveBtn").on("click", function () {
+    // Get nearby values of the description in JQuery
+    var toDo = $(this).siblings(".description").val();
+    var scheduleHour = $(this).parent().attr("id");
+
+    // Save info in local storage
+    localStorage.setItem(scheduleHour, toDo);
+})
+   
+    
