99e8fbe9bcdb51da61c8d8b8cc21d37bdea0e17e
Erin Ewart <ems1980@hotmail.com>
2022-02-26 14:47:23 -0500

    add ability to retrieve from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,5 +15,48 @@ $(".saveBtn").on("click", function () {
     // Save info in local storage
     localStorage.setItem(scheduleHour, toDo);
 })
-   
+
+//Create function to change color of the schedule based on current time
+function changeColor() {
+    var currentHour = moment().hour();
+
+    //Create variable for schedule time to compare to current time
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+        //Add and remove classes based on comparison of current time and schedule time 
+        if (blockTime < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+            
+        }
+        else if (blockTime === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+            
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+
+        }
+    })
+}
+
+// Retrieve items from local storage on page reload
     
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+changeColor();
+
